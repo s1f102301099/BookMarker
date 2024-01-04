7d09907d8171de38418f26b75436864d97f84199
s1f10230014 <131968392+s1f10230014@users.noreply.github.com>
2024-01-04 20:22:56 +0900

    popup記述場所準備

---
+++ book-marker-plasmo/popup/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.css"
+
+function IndexPopup() {
+  const [data, setData] = useState("")
+
+  return <div className={styles.container}>ここにhtml書いてください</div>
+}
+
+export default IndexPopup
