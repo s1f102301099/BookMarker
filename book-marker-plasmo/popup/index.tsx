b949b232da176aae9abc19c96327095045882ddb
Arennn27 <s1f102300087@iniad.org>
2024-01-04 22:09:20 +0900

    html追加

--- book-marker-plasmo/popup/index.tsx
+++ book-marker-plasmo/popup/index.tsx
@@ -3,7 +3,20 @@ import styles from "./index.module.css"
 function IndexPopup() {
   const [data, setData] = useState("")
 
-  return <div className={styles.container}>ここにhtml書いてください</div>
+  return <div className={styles.container}><h1>現在のページを保存しますか</h1></div>
+<div>
+  <div classname=styles.container>
+  <form action='送信先' method='送信メソッド'></form>
+  <input type="submit" value="フォーム"></input>
+  <div></div>
+</div>
+
+<div>
+  <div classname=styles.container></div>
+  <button></button>
+  <div>保存</div>
+</div>
+  
 }
 
 export default IndexPopup
